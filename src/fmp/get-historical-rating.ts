3d7f2cda64388b9fe8830e8af873bf15a6ed70f4
Mig Tam Rod <migtam628@gmail.com>
2021-11-18 15:27:53 -0500

    Create get-historical-rating.ts

---
+++ src/fmp/get-historical-rating.ts
@@ -0,0 +1,42 @@
+// https://financialmodelingprep.com/api/v3/historical-rating/AAPL?limit=100&apikey=705e63025fe8503267b6cbdd8035e7a0
+import axios, { AxiosRequestConfig } from "axios";
+
+const getHistoricalRating = (req: any, res: any) => {
+  let symbol = req.query.symbol;
+  const url =
+    `https://financialmodelingprep.com/api/v3/historical-rating/${symbol.toUpperCase()}?limit=100&apikey=` +
+    process.env.FIN_MOD_PREP_API_KEY;
+  const options: AxiosRequestConfig = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  if (symbol.length > 0) {
+    try {
+      axios
+        .get(url, options)
+        .then((response) => {
+          res.json(response.data);
+        })
+        .catch((error) => {
+          res.json(error);
+        });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        code: 500,
+        message: "Something went wrong",
+        error: error,
+      });
+    }
+  } else {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Please provide a symbol",
+    });
+  }
+};
+
+module.exports = getHistoricalRating;
