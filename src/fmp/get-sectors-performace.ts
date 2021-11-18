134a07cdfb930f9a3cca1207d4b1186f820f6b6f
Mig Tam Rod <migtam628@gmail.com>
2021-11-18 15:28:06 -0500

    Create get-sectors-performace.ts

---
+++ src/fmp/get-sectors-performace.ts
@@ -0,0 +1,33 @@
+// https://financialmodelingprep.com/api/v3/sectors-performance?apikey=2eb9a1881dcf390f428615b17ca72c0a
+import axios, { AxiosRequestConfig } from "axios";
+
+const getSectorsPerformace = (req: any, res: any) => {
+  const url =
+    `https://financialmodelingprep.com/api/v3/sectors-performance?&apikey=` +
+    process.env.FIN_MOD_PREP_API_KEY;
+  const options: AxiosRequestConfig = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  try {
+    axios
+      .get(url, options)
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((error) => {
+        res.json(error);
+      });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Something went wrong",
+      error: error,
+    });
+  }
+};
+
+module.exports = getSectorsPerformace;
