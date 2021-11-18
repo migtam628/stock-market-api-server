865c37749144d384b5e78db55899cb894175c375
Mig Tam Rod <migtam628@gmail.com>
2021-11-18 15:28:16 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 require("dotenv").config();
 import * as functions from "firebase-functions";
 import express from "express";
-import { setRoutes } from "./constants";
-import { initialize } from "./initialize";
+import { initialize, setListenToPort } from "./constants";
+import Routes from "./routes";
 
 const app = express();
 
 initialize(app);
-setRoutes(app);
+Routes.map(({ path, module }: any) => {
+  app.get(path, module);
+});
+setListenToPort(app, process.env.PORT);
 
 export const financialApi = functions.https.onRequest(app);
 // export const financialApiOnCall = functions.https.onCall((data, context) => {
