8cf46bddd3168f007877a573db95d841f63b4c24
Mig Tam Rod <migtam628@gmail.com>
2021-11-18 15:28:21 -0500

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,73 +1,102 @@
-import {
-  TD_GET_FUNDAMENTALS_MODULE,
-  TD_GET_MOVERS_MODULE,
-  TD_GET_PRICE_HISTORY_MODULE,
-  TD_GET_STOCK_MODULE,
-  HOME_MODULE,
-  POLYGON_GET_NEWS_MODULE,
-  POLYGON_GET_COMPANY_DETAILS_MODULE,
-  POLYGON_GET_FINANCIALS_MODULE,
-  POLYGON_GET_MARKET_STATUS_MODULE,
-  FINTEL_GETS_SEC_SHORT_VOL_MODULE,
-  FINTEL_GET_STOCK_FILINGS_MODULE,
-  FINTEL_GET_OWNER_INFO_MODULE,
-  FINTEL_GET_SEC_OWNERSHIP_MODULE,
-  FINTEL_GET_INSTITUTIONAL_HOLDINGS_MODULE,
-  FINTEL_GET_INSIDE_TRADES_MODULE,
-  FINTEL_GET_STOCK_REG_FILINGS_MODULE,
-  POLYGON_GET_STOCK_SPLITS_MODULE,
-  POLYGON_GET_MARKET_HOLIDAYS_MODULE,
-  TD_GET_MOVERS_ROUTE,
-  POLYGON_GET_COMPANY_DETAILS_ROUTE,
-  POLYGON_GET_FINANCIALS_ROUTE,
-  POLYGON_GET_MARKET_HOLIDAYS_ROUTE,
-  POLYGON_GET_MARKET_STATUS_ROUTE,
-  POLYGON_GET_NEWS_ROUTE,
-  POLYGON_GET_STOCK_SPLITS_ROUTE,
-  TD_GET_FUNDAMENTALS_ROUTE,
-  TD_GET_STOCK_ROUTE,
-  TD_GET_PRICE_HISTORY_ROUTE,
-  FINTEL_GETS_SEC_SHORT_VOL_ROUTE,
-  FINTEL_GET_INSTITUTIONAL_HOLDINGS_ROUTE,
-  FINTEL_GET_SEC_OWNERSHIP_ROUTE,
-  FINTEL_GET_STOCK_FILINGS_ROUTE,
-  FINTEL_GET_INSIDE_TRADES_ROUTE,
-  FINTEL_GET_OWNER_INFO_ROUTE,
-  FINTEL_GET_STOCK_REG_FILINGS_ROUTE,
-} from "./constants";
+import HOME_MODULE from "./home";
+import { IRoutes } from "./types";
 
-export function setRoutes(app: any) {
-  
-  app.get("/", HOME_MODULE);
+const Routes: IRoutes = [
+  { path: "/", module: HOME_MODULE },
+  { path: "/td-movers", module: require("./td/get-movers") },
+  { path: "/td-stock", module: require("./td/find-stock") },
+  { path: "/td-fundamentals", module: require("./td/get-fundamentals") },
+  { path: "/td-prices", module: require("./td/get-price-history") },
+  { path: "/poly-news", module: require("./polygon/get-news") },
+  {
+    path: "/poly-company-details",
+    module: require("./polygon/get-company-details"),
+  },
+  { path: "/poly-financials", module: require("./polygon/get-financials") },
+  {
+    path: "/poly-market-status",
+    module: require("./polygon/get-market-status"),
+  },
+  {
+    path: "/poly-stock-splits",
+    module: require("./polygon/get-stock-splits"),
+  },
+  {
+    path: "/poly-market-holidays",
+    module: require("./polygon/get-market-holidays"),
+  },
+  {
+    path: "/fintel-short-volume",
+    module: require("./fintel/get-sec-short-vol"),
+  },
+  {
+    path: "/fintel-stock-filings",
+    module: require("./fintel/get-stock-filings"),
+  },
+  {
+    path: "/fintel-sec-ownership",
+    module: require("./fintel/get-sec-ownership"),
+  },
+  {
+    path: "/fintel-institutional-holdings",
+    module: require("./fintel/get-institutional-holdings"),
+  },
+  { path: "/fintel-owner-info", module: require("./fintel/get-owner-info") },
+  {
+    path: "/fintel-inside-trades",
+    module: require("./fintel/get-inside-trades"),
+  },
+  {
+    path: "/fintel-stock-reg-filings",
+    module: require("./fintel/get-stock-reg-filings"),
+  },
+  { path: "/fmp-filings", module: require("./fmp/get-sec-filings") },
+  { path: "/fmp-float", module: require("./fmp/get-shares-float") },
+  { path: "/fmp-actives", module: require("./fmp/get-most-actives") },
+  {
+    path: "/fmp-growth",
+    module: require("./fmp/get-financial-growth"),
+  },
+  {
+    path: "/fmp-enterprise",
+    module: require("./fmp/get-enterprise-value"),
+  },
+  {
+    path: "/fmp-rating",
+    module: require("./fmp/get-historical-rating"),
+  },
+  {
+    path: "/fmp-crypto",
+    module: require("./fmp/get-crypto-quote"),
+  },
+  {
+    path: "/fmp-gainers",
+    module: require("./fmp/get-gainers"),
+  },
+  {
+    path: "/fmp-losers",
+    module: require("./fmp/get-losers"),
+  },
+  {
+    path: "/fmp-sectors",
+    module: require("./fmp/get-sectors-performace"),
+  },
+  {
+    path: "/fmp-earnings",
+    module: require("./fmp/get-earnings-surprises"),
+  },
+  {
+    path: "/fmp-quote",
+    module: require("./fmp/get-quote"),
+  },
+  {
+    path: "/fmp-chart",
+    module: require("./fmp/get-historical-chart"),
+  },
+  {
+    path: '/fmp-crypto-chart',
+    module: require('./fmp/get-crypto-chart')
+  }
+];
 
-  // TD FUNCTIONS
-  app.get(TD_GET_MOVERS_ROUTE, TD_GET_MOVERS_MODULE);
-  app.get(TD_GET_STOCK_ROUTE, TD_GET_STOCK_MODULE);
-  app.get(TD_GET_FUNDAMENTALS_ROUTE, TD_GET_FUNDAMENTALS_MODULE);
-  app.get(TD_GET_PRICE_HISTORY_ROUTE, TD_GET_PRICE_HISTORY_MODULE);
-  app.get(POLYGON_GET_NEWS_ROUTE, POLYGON_GET_NEWS_MODULE);
-  app.get(
-    POLYGON_GET_COMPANY_DETAILS_ROUTE,
-    POLYGON_GET_COMPANY_DETAILS_MODULE
-  );
-  app.get(POLYGON_GET_FINANCIALS_ROUTE, POLYGON_GET_FINANCIALS_MODULE);
-  app.get(POLYGON_GET_MARKET_STATUS_ROUTE, POLYGON_GET_MARKET_STATUS_MODULE);
-  app.get(POLYGON_GET_STOCK_SPLITS_ROUTE, POLYGON_GET_STOCK_SPLITS_MODULE);
-  app.get(
-    POLYGON_GET_MARKET_HOLIDAYS_ROUTE,
-    POLYGON_GET_MARKET_HOLIDAYS_MODULE
-  );
-  app.get(FINTEL_GETS_SEC_SHORT_VOL_ROUTE, FINTEL_GETS_SEC_SHORT_VOL_MODULE);
-  app.get(FINTEL_GET_STOCK_FILINGS_ROUTE, FINTEL_GET_STOCK_FILINGS_MODULE);
-  app.get(FINTEL_GET_SEC_OWNERSHIP_ROUTE, FINTEL_GET_SEC_OWNERSHIP_MODULE);
-  app.get(
-    FINTEL_GET_INSTITUTIONAL_HOLDINGS_ROUTE,
-    FINTEL_GET_INSTITUTIONAL_HOLDINGS_MODULE
-  );
-  app.get(FINTEL_GET_OWNER_INFO_ROUTE, FINTEL_GET_OWNER_INFO_MODULE);
-  app.get(FINTEL_GET_INSIDE_TRADES_ROUTE, FINTEL_GET_INSIDE_TRADES_MODULE);
-  app.get(
-    FINTEL_GET_STOCK_REG_FILINGS_ROUTE,
-    FINTEL_GET_STOCK_REG_FILINGS_MODULE
-  );
-}
+export default Routes;
