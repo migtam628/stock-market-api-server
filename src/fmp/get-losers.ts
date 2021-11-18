2e9457a490cd382d11cfdbae6e50923f995b7b5c
Mig Tam Rod <migtam628@gmail.com>
2021-11-18 15:27:54 -0500

    Create get-losers.ts

---
+++ src/fmp/get-losers.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosRequestConfig } from "axios";
+https://financialmodelingprep.com/api/v3/actives?apikey=705e63025fe8503267b6cbdd8035e7a0
+//financialmodelingprep.com/api/v3/sec_filings/BBIG?limit=500&apikey=705e63025fe8503267b6cbdd8035e7a0
+
+const getLosers = (req: any, res: any) => {
+  const url =
+    `https://financialmodelingprep.com/api/v3/losers?apikey=` +
+    process.env.FIN_MOD_PREP_API_KEY;
+  const options: AxiosRequestConfig = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+    try {
+      axios
+        .get(url, options)
+        .then((response) => {
+          res.json(response.data);
+        })
+        .catch((error) => {
+          res.json(error);
+        });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        code: 500,
+        message: "Something went wrong",
+        error: error,
+      });
+    }
+};
+
+module.exports = getLosers;
