8badf6f616dec3bc19a1d645f9c30ccaf8ef0af7
Mig Tam Rod <migtam628@gmail.com>
2021-11-19 12:55:59 -0500

    Create get-market-news.ts

---
+++ src/finnhub/get-market-news.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { FINNHUB_API_KEY } from "../constants";
+
+const GetMarketNews = (req: any, res: any) => {
+  let symbol: string | undefined | null = req.query.symbol;
+  let url = `https://finnhub.io/api/v1/news?category=general&token=${FINNHUB_API_KEY}`;
+  if (symbol?.length === 0) {
+    res.status(400).json({
+      message: "Please provide a symbol",
+    });
+  } else {
+    try {
+      axios
+        .get(url)
+        .then((response: any) => {
+          res.status(200).json(response.data);
+        })
+        .catch((error) => {
+          res.status(400).json({
+            message: error.message,
+          });
+        });
+    } catch (error) {
+      res.status(400).json({
+        error,
+      });
+    }
+  }
+};
+
+module.exports = GetMarketNews;
