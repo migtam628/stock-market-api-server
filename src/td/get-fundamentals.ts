4f7dbc27b007fe1f1f205721720ea7170a63e7b3
Mig Tam Rod <migtam628@gmail.com>
2022-01-24 22:35:47 -0500

    Update get-fundamentals.ts

--- src/td/get-fundamentals.ts
+++ src/td/get-fundamentals.ts
@@ -1,7 +1,28 @@
 import axios from "axios";
 import { TD_API_KEY } from "../constants";
 
-module.exports = (req: any, res: any) => {
+/**
+ *
+ *  @param {string} symbol
+ * @returns {void}
+ * @beta
+ * @example
+ * ```
+ * const { GetFundamentals } = require("@tda-api/client");
+ * GetFundamentals("AAPL");
+ * ```
+ * @example
+ * ```
+ * const app = require('express')()
+ * app.get("/api/fundamentals", require("PATH_TO/get-fundamentals"));
+ * app.listen(3000)
+ *
+ * "https://localhost:3000/api/fundamentals?symbol=AAPL"
+ *
+ *
+ *
+ */
+const GetFundamentals = (req: any, res: any) => {
   let symbol = req.query.symbol;
   let url = `https://api.tdameritrade.com/v1/instruments?apikey=${TD_API_KEY}&symbol=${symbol}&projection=fundamental`;
   if (symbol.length > 0) {
@@ -10,9 +31,11 @@ module.exports = (req: any, res: any) => {
         .get(url)
         .then((response) => {
           res.json(response.data);
+          return response.data;
         })
         .catch((error) => {
           res.json(error);
+          return error;
         });
     } catch (error) {
       res.json(error);
@@ -20,5 +43,5 @@ module.exports = (req: any, res: any) => {
   }
 };
 
-
+module.exports = GetFundamentals;
 // https://api.tdameritrade.com/v1/instruments?apikey=${TD_API_KEY}&symbol=bbig&projection=fundamental
