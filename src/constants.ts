1eb3f318a16e56da00910bc31aab678ecfab4704
Mig Tam Rod <migtam628@gmail.com>
2022-01-25 22:53:14 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -8,4 +8,5 @@ export const TD_API_KEY = process.env.TD_API_KEY,
   FINNHUB_API_KEY = process.env.FINNHUB_API_KEY,
   POLYGON_API_KEY = process.env.POLYGON_API_KEY,
   FIN_MOD_PREP_API_KEY = process.env.FIN_MOD_PREP_API_KEY,
+  TIINGO_API_KEY = process.env.TIINGO_API_KEY,
   PORT = process.env.PORT;
