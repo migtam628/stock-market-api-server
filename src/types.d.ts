6818398cc8b914b53e357d547d5a3c734d456fdc
Mig Tam Rod <migtam628@gmail.com>
2022-01-27 17:02:50 -0500

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,6 @@
-export interface Mover {
+declare module "sec-api";
+
+interface Mover {
   change: number;
   description: string;
   direction: string;
@@ -7,9 +9,66 @@ export interface Mover {
   totalVolume: number;
 }
 
-export interface IRoute {
+interface IRoute {
   path: string;
   module: any;
 }
 
-export type IRoutes = IRoute[];
+type IRoutes = IRoute[];
+
+type TEndpoints = {
+  [key in EndpointChoices]?: IEndpoints;
+};
+
+interface IEndpoints {
+  name: string;
+  path: string;
+  example: string;
+}
+
+enum EndpointChoices {
+  finnMarketNews = "finnMarketNews",
+  finnIpoCalendar = "finnIpoCalendar",
+  finnEarnings = "finnEarnings",
+  finnStockMetrics = "finnStockMetrics",
+  finnStockNews = "finnStockNews",
+  finnSecFilings = "finnSecFilings",
+  finnSimilar = "finnSimilar",
+  finnStockProfile = "finnStockProfile",
+  finnSearch = "finnSearch",
+  tiingoMarketNews = "tiingoMarketNews",
+  tiingoDailyPrices = "tiingoDailyPrices",
+  tiingoCryptoPrices = "tiingoCryptoPrices",
+  finnNews = "finnNews",
+  tdMovers = "tdMovers",
+  tdStocks = "tdStocks",
+  tdFundamentals = "tdFundamentals",
+  tdPrices = "tdPrices",
+  polyNews = "polyNews",
+  polyCompanyDetails = "polyCompanyDetails",
+  polyFinancials = "polyFinancials",
+  polyMarketStatus = "polyMarketStatus",
+  polyStockSplits = "polyStockSplits",
+  polyMarketHolidays = "polyMarketHolidays",
+  fintelShortVolume = "fintelShortVolume",
+  fintelStockFilings = "fintelStockFilings",
+  fintelSecOwnership = "fintelSecOwnership",
+  fintelInstitutionalHolding = "fintelInstitutionalHolding",
+  fintelOwnerInfo = "fintelOwnerInfo",
+  fintelInsideTrades = "fintelInsideTrades",
+  fintelStockRegFiling = "fintelStockRegFiling",
+  fmpFilings = "fmpFilings",
+  fmpFloat = "fmpFloat",
+  fmpActives = "fmpActives",
+  fmpGrowth = "fmpGrowth",
+  fmpEnterprise = "fmpEnterprise",
+  fmpRating = "fmpRating",
+  fmpCrypto = "fmpCrypto",
+  fmpSectors = "fmpSectors",
+  fmpLosers = "fmpLosers",
+  fmpGainers = "fmpGainers",
+  fmpEarnings = "fmpEarnings",
+  fmpQuote = "fmpQuote",
+  fmpChart = "fmpChart",
+  fmpCryptoChart = "fmpCryptoChart",
+}
