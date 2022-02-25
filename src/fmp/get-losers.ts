78ca068bfb79cef834759d81190a19b09fe861f8
Mig Tam Rod <migtam628@gmail.com>
2022-02-25 01:02:49 -0500

    Update get-losers.ts

--- src/fmp/get-losers.ts
+++ src/fmp/get-losers.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
-https://financialmodelingprep.com/api/v3/actives?apikey=705e63025fe8503267b6cbdd8035e7a0
+// https://financialmodelingprep.com/api/v3/actives?apikey=705e63025fe8503267b6cbdd8035e7a0
 //financialmodelingprep.com/api/v3/sec_filings/BBIG?limit=500&apikey=705e63025fe8503267b6cbdd8035e7a0
 
 const getLosers = (req: any, res: any) => {
